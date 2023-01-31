ab3a9d87aedb1ff54a71a2eafec5e12ce38cb992
venghadakrishnan <venghada@gmail.com>
2023-01-31 15:47:08 +0530

    java programs

---
+++ pali.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class pali{
+    public static void main(String[] args){
+        String str,rev=" ";
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter a string");
+        str = obj.nextLine();
+
+        int l = str.length();
+
+        for(int i = l-1;i >= 0;i--)
+            rev = rev+str.charAt(i);
+            
+        if(str.equals(rev))
+        System.out.println("palindrome");
+        else
+        System.out.println("not a palindrome");
+
+        
+    }
+}
+
---
+++ strrev.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class strrev {
+    public static void main(String[] args){
+
+    String str , rev="";
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter a string:");
+    str = sc.nextLine();
+
+    for(int i =str.length()-1;i>=0;i--)
+        rev = rev+str.charAt(i);
+    
+    System.out.print(rev);
+
+    
+    }
+    
+    
+}
